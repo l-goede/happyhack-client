1694e10898608ccd08d66137cbfcbe597b7f0cf7
Fern Alves <fernandapontual5@gmail.com>
2021-12-16 04:25:48 +0100

    homepaghe done! needs ajust responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,16 @@ import axios from "axios";
 function Home() {
 
 const [someJson, setJson] = useState(null)
+const [someJsonTwo, setJsonTwo] = useState(null)
+const [someJsonThree, setJsonThree] = useState(null)
+
+useEffect(() => {
+  const getData = async () => {
+    let res = await axios.get("https://assets1.lottiefiles.com/packages/lf20_GcNGbf.json")
+    setJsonThree(res.data)
+  }
+  getData()
+}, [])
 
 useEffect(() => {
   const getData = async () => {
@@ -16,29 +26,37 @@ useEffect(() => {
   getData()
 }, [])
 
-if (!someJson) {
-    return <h1>Loading</h1>;
+useEffect(() => {
+  const getData = async () => {
+    let res = await axios.get("https://assets8.lottiefiles.com/packages/lf20_rxe6lehm.json")
+    setJsonTwo(res.data)
   }
+  getData()
+}, [])
 
-  return (
-<div> 
+if (!someJson || !someJsonTwo || !someJsonThree ) {
+    return <h1>Loading</h1>;
+}
+
+return (
+<div className="paddingHomepage"> 
 
 
 <div className="area-1" >
 <span> <br/>  </span>
-<span className="square-text-area-1">
+<div className="area-1-wrapper" >
 
-<h1> Get help, get paid and share your skills </h1> 
-<p> Need an extra pair of hands to get you javaScript skills a bit more polished? Or maybe you are a front end who needs to develop your back end side of your website? We've got you covered. </p>
+<div className="square-text-area-1">
+<h1> Get help and get paid for your skills </h1> 
+<h5> Need an extra pair of hands to get you javaScript skills a bit more polished? <br/> Or maybe you are a front end who needs to develop your back end side of your website? <br/> We've got you covered. </h5>
+</div>
 <div className="lottieGreenWrapper"> 
-<div classname="lottieGreen"><LottieControl animation={someJson} width={500} height={500}/> </div>
+<span classname="lottieGreen"><LottieControl animation={someJson} width={400} height={400}/> </span>
+</div>
+</div>
 </div>
 
-
-</span>
-</div>
-
-<div className="area-2">
+<div className="area-21">
 <span> <br/>  </span>
 
 
@@ -59,8 +77,8 @@ if (!someJson) {
 
 <div className="square-text">
 <h1> 💃 </h1>
-<h1> Fun code </h1> 
-<p> Find developers willing to give you a hand. </p>
+<h1> Have fun </h1> 
+<p> Hava a stress-free coding experience.   </p>
 </div>
 
 </div>
@@ -72,13 +90,11 @@ if (!someJson) {
 <div className="area-3-div">
 <div className="area-3-left">
 <span> <br/>  </span>
-<span className="square-text">
 
 
-
-{/* <img id="codingirl" src= "/codingirl.png"  alt="Girl in a jacket" /> */}
-
-</span>
+<div> 
+<span ><LottieControl animation={someJsonTwo} width={410} height={350}/> </span>
+</div>
 </div>
 
 <div className="area-4-right" >
@@ -92,32 +108,14 @@ if (!someJson) {
 
 
 <div className="area-5" >
-<span> <br/>  </span>
-<span className="square-text">
-<h1> We are happyhack! </h1> 
-<p> A happy coding society. Created by Web Developement students. </p>
+<span> <br/> </span>
+
+<span ><LottieControl animation={someJsonThree} width={350} height={350}/> </span>
+<span className="centered">
+<h3> A happy coding community. Created by Web Developement students. </h3>
 </span>
+
 </div>
-
-
- {/* <div className="area-3-div">
-<div className="area-5-left">
-<span> <br/>  </span>
-<span className="square-text">
-<img id="codingirl" src= "/codingirl.png"  alt="Girl in a jacket" />
-</span>
-</div>
-
-<div className="area-6-right" >
-<span><br/></span>
-<span className="square-text">
-<h1> Chill with us</h1> 
-</span>
-</div> 
-
-</div> */}
-
-
 
 </div>
 
