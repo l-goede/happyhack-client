0c83a0f82b2b9ba8aaead1e2e13f85d6f28cd046
l-goede <luise.goede@yahoo.de>
2021-12-29 15:59:52 +0100

    comment out localhost in config js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-//const API_URL = "https://happyhack01.herokuapp.com/api";
-//const SOCKET_URL = "https://happyhack01.herokuapp.com";
+const API_URL = "https://happyhack01.herokuapp.com/api";
+const SOCKET_URL = "https://happyhack01.herokuapp.com";
 
-const API_URL = "http://localhost:5005/api"
-const SOCKET_URL = "http://localhost:5005"
+//const API_URL = "http://localhost:5005/api"
+//const SOCKET_URL = "http://localhost:5005"
 
 export { API_URL, SOCKET_URL };
